4db34335bdd02cf8612481216de645ac286e3a99
LordMushroom1 <lordmushon1@gmail.com>
2024-12-25 01:52:18 +0200

    Account API

--- BANK/Data/Entities/Account.cs
+++ BANK/Data/Entities/Account.cs
@@ -14,8 +14,8 @@
         public Currency Currency { get; set; }
 
         public string ClientId { get; set; }
-        public ApplicationUser Client { get; set; }
+        public ApplicationUser? Client { get; set; }
 
-        public ICollection<Card> Cards { get; set;}
+        public ICollection<Card>? Cards { get; set;}
     }
 }
